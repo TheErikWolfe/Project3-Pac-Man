7033cc5f687a2702249845c6db86f9b33ab69708
Erik Wolfe <theerikwolfe@gmail.com>
2018-06-13 10:20:54 -0400

    Make Pacman shape controllable

--- app/main.js
+++ app/main.js
@@ -1,22 +1,51 @@
 // comment
 var canvas = document.querySelector('canvas');
 
-canvas.width = 500;
-canvas.height = 700;
+canvas.width = 300;
+canvas.height = 300;
 
 var c = canvas.getContext('2d');
 
-window.addEventListener("keypress", keyPressed);  // bind to taskInput, not addButton
+// left, up, right, down
+var directions = [false, false, false, false];
 
-function keyPressed(k) 
-{
-  if (k.code == 'Enter')
-  {     // only if the key is "Enter"...
-    addTask();    
-  }
-              // ...add a new task (using same handler as the button)
-  return false;               // no propagation or default
+document.onreadystatechange = function() {
+	if (document.readyState == "interactive") {
+		//Initialize buttons
+		window.addEventListener('keydown', arrowKeysLogic);
+		//window.addEventListener('keyup', buttonReleaseLogic);
+	}
 }
+
+function arrowKeysLogic(){
+	var i = 0;
+	if (event.keyCode == 37) 
+	{
+		i = 0;
+	}
+	else if (event.keyCode == 38) 
+	{
+		i = 1;
+	}
+	else if (event.keyCode == 39) 
+	{
+		i = 2;
+	}
+	else if (event.keyCode == 40) 
+	{
+		i = 3;
+	}
+	setDirection(i);
+	console.log(i);
+}
+
+function setDirection(i)
+{
+	directions = [false, false, false, false];
+	directions[i] = [true];
+}
+
+
 
 pac = new Pacman(50,50, 3, 3, 30);
 
@@ -40,9 +69,68 @@ function Pacman(x, y, dx, dy, radius)
 	}
 	this.update = function()
 	{
-		this.x += this.dx;
+/*		if(this.x + this.radius > canvas.width || this.x - this.radius < 0)
+		{
+			directions = [false, false, false, false];
+		}
+		else if(this.y + this.radius > innerHeight || this.y - this.radius < 0)
+		{
+			directions = [false, false, false, false];
+		}
+		else
+		{
+			this.move();
+		}
+*/
+		var pad = 2.25;
+		if(directions[0] || directions[2])
+		{
+			if(this.x + this.radius + pad > canvas.width)
+			{
+				directions[2] = false;
+			}
+			else if(this.x - this.radius - pad < 0)
+			{
+				directions[0] = false;
+			}
+		}
+		else if( directions[1] || directions[3])
+		{
+			if(this.y + this.radius + pad > canvas.height)
+			{
+				directions[3] = false;
+			}
+			else if(this.y - this.radius - pad < 0)
+			{
+				directions[1] = false;
+			}
+		}
+		this.move();
+		
+
 		this.draw();
-		console.log("Made It: " + this.x );
+		//console.log("Made It: " + this.x );
+	}
+
+	this.move = function()
+	{
+		// left up right down
+		if(directions[0])
+		{
+			this.x -= this.dx;
+		}
+		else if(directions[1])
+		{
+			this.y -= this.dy;		
+		}
+		else if(directions[2])
+		{
+			this.x += this.dx;
+		}
+		else if(directions[3])
+		{
+			this.y += this.dy;
+		}
 	}
 }
 
